839a3f864c6bc4ff9e1dbc60b1913a4448fb3287
ShelterRin <ShelterRin@github.com>
2024-10-09 23:51:52 +0800

    Added local shock API.

--- shocking_vrchat.py
+++ shocking_vrchat.py
@@ -1,6 +1,6 @@
 from typing import List
 import asyncio
-import yaml, uuid, os, sys, traceback, time, socket
+import yaml, uuid, os, sys, traceback, time, socket, re, json
 from threading import Thread
 from loguru import logger
 import traceback
@@ -36,6 +36,7 @@ SETTINGS_BASIC = {
         },
         'channel_b': {
             'avatar_params': [
+                '/avatar/parameters/pcs/contact/enterPass',
                 '/avatar/parameters/lms-penis-proximityA*',
                 '/avatar/parameters/Shock/TouchAreaB',
                 '/avatar/parameters/Shock/TouchAreaC',
@@ -128,6 +129,62 @@ def get_conns():
 @app.route('/sendwav')
 async def sendwav():
     await DGConnection.broadcast_wave(channel='A', wavestr=srv.waveData[0])
+    return 'OK'
+
+@app.route('/api/v1/shock/<channel>/<second>')
+async def api_v1_shock(channel, second):
+    if channel == 'all':
+        channels = ['A', 'B']
+    else:
+        channels = [channel]
+    try: 
+        second = float(second)
+    except Exception:
+        logger.warning('[API][shock] Invalid second, set to 1.')
+        second = 1.0
+    second = min(second, 10.0)
+    repeat = int(10 * second)
+    for _ in range(repeat // 10):
+        for chan in channels:
+            await api_v1_sendwave(chan, 10, '0A0A0A0A64646464')
+    if repeat % 10 > 0:
+        for chan in channels:
+            await api_v1_sendwave(chan, repeat % 10, '0A0A0A0A64646464')
+    return 'OK'
+
+@app.route('/api/v1/sendwave/<channel>/<repeat>/<wavedata>')
+async def api_v1_sendwave(channel, repeat, wavedata):
+    """API V1 Sendwave.
+
+    Keyword arguments:
+    channel -- A or B.
+    repeat -- repeat times, 1 for 100ms, 1 to 80. Max 80 for json length limit.
+    wavedata -- Coyote v3 wave format, eg. 0A0A0A0A64646464.
+    """
+    try:
+        channel = channel.upper()
+        if channel not in ['A', 'B']:
+            raise Exception
+    except:
+        logger.warning('[API][sendwave] Invalid Channel, set to A.')
+        channel = 'A'
+    try:
+        repeat = int(repeat)
+        if repeat > 100 or repeat < 1:
+            raise Exception
+    except:
+        logger.warning('[API][sendwave] Invalid repeat times, set to 10.')
+        repeat = 10
+    try:
+        if not re.match(r'^([0-9A-F]{16})$', wavedata):
+            raise Exception
+    except:
+        logger.warning('[API][sendwave] Invalid wave, set to 0A0A0A0A64646464.')
+        wavedata = '0A0A0A0A64646464'
+    wavestr = [wavedata for _ in range(repeat)]
+    wavestr = json.dumps(wavestr, separators=(',', ':'))
+    logger.success(f'[API][sendwave] C:{channel} R:{repeat} W:{wavedata}')
+    await DGConnection.broadcast_wave(channel=channel, wavestr=wavestr)
     return 'OK'
 
 def strip_basic_settings(settings: dict):
@@ -243,16 +300,17 @@ def main():
             logger.success(f"Channel {chann} Mode：{chann_mode} Listening：{param}")
             dispatcher.map(param, shock_handler.osc_handler)
     
-    TuyaConn = TuYaConnection(
-        access_id=SETTINGS['machine']['tuya']['access_id'],
-        access_key=SETTINGS['machine']['tuya']['access_key'],
-        device_ids=SETTINGS['machine']['tuya']['device_ids'],
-    )
-    machine_tuya_handler = TuyaHandler(SETTINGS=SETTINGS, DEV_CONN=TuyaConn)
-    handlers.append(machine_tuya_handler)
-    for param in SETTINGS['machine']['tuya']['avatar_params']:
-        logger.success(f"Machine Listening：{param}")
-        dispatcher.map(param, machine_tuya_handler.osc_handler)
+    if 'machine' in SETTINGS and 'tuya' in SETTINGS['machine']:
+        TuyaConn = TuYaConnection(
+            access_id=SETTINGS['machine']['tuya']['access_id'],
+            access_key=SETTINGS['machine']['tuya']['access_key'],
+            device_ids=SETTINGS['machine']['tuya']['device_ids'],
+        )
+        machine_tuya_handler = TuyaHandler(SETTINGS=SETTINGS, DEV_CONN=TuyaConn)
+        handlers.append(machine_tuya_handler)
+        for param in SETTINGS['machine']['tuya']['avatar_params']:
+            logger.success(f"Machine Listening：{param}")
+            dispatcher.map(param, machine_tuya_handler.osc_handler)
 
 
     th = Thread(target=async_main_wrapper, daemon=True)
